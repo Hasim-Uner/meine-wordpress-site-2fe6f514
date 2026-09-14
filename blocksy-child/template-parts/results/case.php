f418c554b4f2c69080ad4833aa37385fb6160af5
Haşim Üner <hasimuener@gmail.com>
2026-09-14 20:50:15 +0200

    Add results case section shell

---
+++ blocksy-child/template-parts/results/case.php
@@ -0,0 +1,12 @@
+<?php
+/** Results hub: documented outcome proof. */
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+?>
+<section id="grossprojekt" aria-labelledby="grossprojekt-h" data-track-section="grossprojekt">
+	<div class="blatt reihe">
+		<div class="spalte-links"><div class="kapitel"><span class="nr">01</span><span class="titel">Dokumentierter Fall</span><span class="strich" aria-hidden="true"></span></div></div>
+		<div class="voll">
+			<h2 class="kopf" id="grossprojekt-h">Von der Website bis zur Übergabe an den Vertrieb.</h2>
+		</div>
+	</div>
+</section>
